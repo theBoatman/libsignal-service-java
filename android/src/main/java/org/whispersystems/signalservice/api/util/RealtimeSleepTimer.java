0e82fcccc38df2053f7ce8cd00c77a99944f91ca
theBoatman <theBoatman@weihs.org>
2019-03-18 22:16:52 +0100

    fixing sleep for multiple threads

--- android/src/main/java/org/whispersystems/signalservice/api/util/RealtimeSleepTimer.java
+++ android/src/main/java/org/whispersystems/signalservice/api/util/RealtimeSleepTimer.java
@@ -12,6 +12,7 @@ import android.util.Log;
 
 import org.whispersystems.signalservice.api.util.SleepTimer;
 
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,25 +21,38 @@ import java.util.concurrent.TimeUnit;
  *
  */
 public class RealtimeSleepTimer implements SleepTimer {
+
   private static final String TAG = RealtimeSleepTimer.class.getSimpleName();
 
-  private final AlarmReceiver alarmReceiver;
+  static {
+    Log.w(TAG, "theBoatman: fixed parallel sleep");
+  }
+
+
+  private static ConcurrentSkipListSet<Integer> actionIdList = new ConcurrentSkipListSet<>();
+
   private final Context context;
 
   public RealtimeSleepTimer(Context context) {
     this.context = context;
-    alarmReceiver = new RealtimeSleepTimer.AlarmReceiver();
   }
 
   @Override
   public void sleep(long millis) {
-    context.registerReceiver(alarmReceiver,
-                             new IntentFilter(AlarmReceiver.WAKE_UP_THREAD_ACTION));
+    final AlarmReceiver alarmReceiver = new RealtimeSleepTimer.AlarmReceiver();
+    int actionId = 0;
+    while (!actionIdList.add(actionId)){
+      actionId++;
+    }
+    Log.w(TAG, "Sleep Action ID: "+ actionId +". Thread: " + Thread.currentThread().getName());
+    try {
+      context.registerReceiver(alarmReceiver,
+              new IntentFilter(AlarmReceiver.WAKE_UP_THREAD_ACTION + "." + actionId));
 
-    final long startTime = System.currentTimeMillis();
-    alarmReceiver.setAlarm(millis);
+      final long startTime = System.currentTimeMillis();
+      alarmReceiver.setAlarm(millis, AlarmReceiver.WAKE_UP_THREAD_ACTION + "." + actionId);
 
-    while (System.currentTimeMillis() - startTime < millis) {
+      while (System.currentTimeMillis() - startTime < millis) {
         try {
           synchronized (this) {
             wait(millis - System.currentTimeMillis() + startTime);
@@ -46,39 +60,43 @@ public class RealtimeSleepTimer implements SleepTimer {
         } catch (InterruptedException e) {
           Log.w(TAG, e);
         }
+      }
+      context.unregisterReceiver(alarmReceiver);
+    } catch(Exception e) {
+      Log.w(TAG, "Exception during sleep ...",e);
+    }finally {
+      actionIdList.remove(actionId);
     }
-
-    context.unregisterReceiver(alarmReceiver);
   }
 
   private class AlarmReceiver extends BroadcastReceiver {
     private static final String WAKE_UP_THREAD_ACTION = "org.whispersystems.signalservice.api.util.RealtimeSleepTimer.AlarmReceiver.WAKE_UP_THREAD";
 
-    private void setAlarm(long millis) {
-      final Intent        intent        = new Intent(WAKE_UP_THREAD_ACTION);
+    private void setAlarm(long millis, String LABEL) {
+      final Intent        intent        = new Intent(LABEL);
       final PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
       final AlarmManager  alarmManager  = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 
-      Log.w(TAG, "Setting alarm to wake up in " + millis + "ms.");
+      Log.w(TAG, "Setting alarm to wake up in " + millis + "ms. Thread: " + Thread.currentThread().getName());
 
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
         alarmManager.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                                               SystemClock.elapsedRealtime() + millis,
-                                               pendingIntent);
+                SystemClock.elapsedRealtime() + millis,
+                pendingIntent);
       } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
         alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                              SystemClock.elapsedRealtime() + millis,
-                              pendingIntent);
+                SystemClock.elapsedRealtime() + millis,
+                pendingIntent);
       } else {
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                         SystemClock.elapsedRealtime() + millis,
-                         pendingIntent);
+                SystemClock.elapsedRealtime() + millis,
+                pendingIntent);
       }
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-      Log.w(TAG, "Waking up.");
+      Log.w(TAG, "Waking up. Thread: " + Thread.currentThread().getName());
 
       synchronized (RealtimeSleepTimer.this) {
         RealtimeSleepTimer.this.notifyAll();
